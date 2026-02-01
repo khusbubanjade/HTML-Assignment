90e6e7399f8d5199f27d2cb363d145225dc48b91
khusbubanjade <khusbubanjade7@gmail.com>
2026-02-01 10:15:53 +0545

    color changer update

--- dynamic color changer assignment/index.js
+++ dynamic color changer assignment/index.js
@@ -1,15 +1,16 @@
-function b1(){
-    document.getElementsByTagName('body')[0].style.backgroundColor='#00ffff'
-} 
-function b2(){
-    document.getElementsByTagName('body')[0].style.backgroundColor='#7fffd4'
-} 
-function b3(){
-    document.getElementsByTagName('body')[0].style.backgroundColor='#ffc0cb'
-} 
-function b4(){
-    document.getElementsByTagName('body')[0].style.backgroundColor='#deb887'
-} 
-function b5(){
-    document.getElementsByTagName('body')[0].style.backgroundColor='#808080'
-} 
+const colorBox = document.querySelector('.color-box');
+const colorPicker = document.getElementById('colorPicker');
+const resetBtn = document.getElementById('resetBtn');
+
+const defaultColor = '#ffffff'; // default background color
+
+
+colorPicker.addEventListener('input', () => {
+    colorBox.style.backgroundColor = colorPicker.value;
+});
+
+
+resetBtn.addEventListener('click', () => {
+    colorBox.style.backgroundColor = defaultColor;
+    colorPicker.value = '#ffffff'; // also reset the color picker
+});
