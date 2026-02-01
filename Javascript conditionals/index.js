98c2101904b50220c0ac0ab9c4d4c9759d445402
khusbubanjade <khusbubanjade7@gmail.com>
2026-02-01 09:25:04 +0545

    color changer assignment

---
+++ Javascript conditionals/index.js
@@ -0,0 +1,6 @@
+let num =  prompt("Enter a number: ")
+if (num%2==0){
+    print("The given number is even")
+} else {
+    print("The number is odd.")
+}
---
+++ dynamic color changer assignment/index.js
@@ -0,0 +1,15 @@
+function b1(){
+    document.getElementsByTagName('body')[0].style.backgroundColor='#00ffff'
+} 
+function b2(){
+    document.getElementsByTagName('body')[0].style.backgroundColor='#7fffd4'
+} 
+function b3(){
+    document.getElementsByTagName('body')[0].style.backgroundColor='#ffc0cb'
+} 
+function b4(){
+    document.getElementsByTagName('body')[0].style.backgroundColor='#deb887'
+} 
+function b5(){
+    document.getElementsByTagName('body')[0].style.backgroundColor='#808080'
+} 
